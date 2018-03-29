9dd6232313ff3b792318d5dd990919e6cc7abf80
EwanJC123 <37845741+EwanJC123@users.noreply.github.com>
2018-03-29 22:15:33 +0100

    Board Generation V2
    Board Generation with Check Board, Pawn Generation and Castle Generation

--- tKinter/tKinter/Board Generation.py
+++ tKinter/tKinter/Board Generation.py
@@ -4,17 +4,35 @@ root=Tk()
 
 canvas=Canvas(root, width = 640, height = 640)
 canvas.pack()
-count=0
-for i in range(0,8):
-    
+blockWidth=640//8
+blockHeight=640//8
+pawnCount=0
+castleCount=0
+for row in range(0,8):#Check Board Generation
+    for column in range(0,8):
+        if (row+column)%2==0:
+            canvas.create_rectangle(column*blockWidth,row*blockHeight,(column+1)*blockWidth,(row+1)*blockHeight, fill="#f7f4ad")
+        else:
+            canvas.create_rectangle(column*blockWidth,row*blockHeight,(column+1)*blockWidth,(row+1)*blockHeight, fill="#af8d69")
+
+
+
+for i in range(0,8):#Grid GUI Generation
     canvas.create_line(80*i,0,80*i,640)
     canvas.create_line(0,80*i,640,80*i)
-    canvas.create_line(0+count,80,80+count,160)
-    canvas.create_line(0+count,560,80+count,480)
-    count+=80
 
 
+ 
+for pawnGen in range(0,8):#Pawn GUI Generation
+    canvas.create_line(10+pawnCount,90,80+pawnCount-10,150)
+    canvas.create_line(10+pawnCount,550,80+pawnCount-10,490)
+    pawnCount+=80
 
+
+for castleGen in range(0,2):#Rook GUI Generator
+    canvas.create_line(40+castleCount,630,40+castleCount,570)
+    canvas.create_line(40+castleCount,10,40+castleCount,70)
+    castleCount+=560
 
 
 mainloop()
